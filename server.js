77e1274a75b51e535e9bb9e409e2c5a28bd43d02
Ansab Naif <ansab20556@gmail.com>
2025-09-22 23:19:10 +0300

    feat: Implement asynchronous HTTP server to serve HTML

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Server Error');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        }
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
